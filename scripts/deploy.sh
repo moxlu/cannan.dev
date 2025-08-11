995867aa0e46b5f0a09c75120e9083a9be46849e
Lee <moxlu@pm.me>
2025-08-11 21:17:38 +1000

    how the turn tables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,51 +1,49 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y sqlite3 certbot git wget golang-go
+
 # setup sshd
 # setup ufw
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y golang-go sqlite3 certbot git
-
-if ! id -u cannanbot > /dev/null 2>&1; then
-    sudo useradd -r -s /bin/false cannanbot
-fi
-sudo groupadd ssl-cert
-sudo usermod -aG ssl-cert cannanbot
-
 sudo certbot certonly --standalone -d cannan.dev --non-interactive --agree-tos -m certs@cannan.dev
-sudo chgrp -R ssl-cert /etc/letsencrypt/live/cannan.dev
-sudo chgrp -R ssl-cert /etc/letsencrypt/archive/cannan.dev
-sudo chmod -R 750 /etc/letsencrypt/live/cannan.dev
-sudo chmod -R 750 /etc/letsencrypt/archive/cannan.dev
-sudo find /etc/letsencrypt/live/cannan.dev -type f -exec chmod 640 {} \;
-sudo find /etc/letsencrypt/archive/cannan.dev -type f -exec chmod 640 {} \;
+sudo chgrp -R ssl-cert /etc/letsencrypt/
+sudo chmod -R 750 /etc/letsencrypt/
+sudo find /etc/letsencrypt/ -type f -exec chmod 640 {} \;
 
 cd /opt/
 if [ ! -d "cannan.dev" ]; then
     sudo git clone https://github.com/moxlu/cannan.dev.git
 fi
 
-cd /opt/cannan.dev
-sudo -u cannanbot go build -o cannanapp ./src
-sudo mkdir -p run challengeFiles
-sudo chown -R cannanbot:cannanbot /opt/cannan.dev
-
+sudo mkdir -p /opt/cannan.dev/run
 cd /opt/cannan.dev/run
+
+sudo wget -O /opt/cannan.dev/run/cannanapp https://github.com/moxlu/cannan.dev/releases/download/cannanapp/cannanapp
+sudo chmod +x /opt/cannan.dev/cannanapp
+sudo setcap 'cap_net_bind_service=+ep' /opt/cannan.dev/run/cannanapp # allows priv ports
+
 sudo ln -s /etc/letsencrypt/live/cannan.dev/fullchain.pem ./fullchain.pem
 sudo ln -s /etc/letsencrypt/live/cannan.dev/privkey.pem ./privkey.pem
 sudo sqlite3 cannan.db < ../scripts/schema.sql
 
+# from dev: scp and mv init.sql and challengeFiles
+
+cd /opt/cannan.dev/run
+sudo sqlite3 cannan.db < init.sql
+
 cd /opt/cannan.dev/scripts
 sudo ./generate_session_key.sh
 sudo cp cannan.service /etc/systemd/system/cannan.service
-sudo cp restart_cannan.sh /etc/letsencrypt/renewal-hooks/deploy/restart-cannan.sh
+sudo cp restart_cannan.sh /etc/letsencrypt/renewal-hooks/deploy/restart_cannan.sh
 sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/restart_cannan.sh
 
+sudo useradd -r -s /bin/false cannanbot
+sudo groupadd ssl-cert
+sudo usermod -aG ssl-cert cannanbot
 sudo chown -R cannanbot:cannanbot /opt/cannan.dev
 
 sudo systemctl daemon-reload
 sudo systemctl enable cannan.service
 sudo systemctl start cannan.service
-
-# Don't forget to scp init.sql and challengeFiles
